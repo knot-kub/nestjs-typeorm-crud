7a9422a0b8a5b1dc46a8492f7ca29ed659e94022
Swiss Lertpattanasak <swiss.knot@gmai.com>
2024-07-23 10:08:09 +0700

    feat: add response interceptor for format response

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import configuration from './config/configuration'
+import { TestModule } from './test/test.module'
+import { ResponseInterceptor } from './interceptor/response.interceptor'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 
 @Module({
   imports: [
@@ -12,8 +15,14 @@ import configuration from './config/configuration'
       useFactory: async (configService: ConfigService) =>
         configService.get('database').options,
     }),
+    TestModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
+  ],
 })
 export class AppModule {}
